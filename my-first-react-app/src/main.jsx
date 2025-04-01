9ee4007a8615b5b46c330e0013d5d9f4b0613dae
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-01 16:22:21 +0530

    added search bar

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App }from './App.jsx'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
     <App />
-  </React.StrictMode>,
 )
