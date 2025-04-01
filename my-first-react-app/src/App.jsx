9090ad288e704c01d83cdfe6d732c7d8fe70fbe5
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-01 16:19:29 +0530

    added images

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { useState } from 'react';
+import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx'
+import MovieCard from './components/MovieCard.jsx'
 
-export const App = () =>{
+const App = () =>{
+    const [searchTerm, setSearchTerm] = useState('');
     return (
         <main>
             <div className="pattern" />
             <div className="wrapper"> 
                 <header>
-                    <h1> Find Movies that you enjoy </h1>
+                    <img src="./hero.png" alt="Hero Banner" />
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
+                <Search />
             </div>
         </main>
     )
 
 }
+
+export default App
